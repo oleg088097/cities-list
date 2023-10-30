8e53f6b72226c08fb0d119d41cd07e14c055ab1b
Oleg Kuzmin <kuzminoo@biocad.ru>
2023-10-30 09:38:15 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { CdkTreeModule } from "@angular/cdk/tree";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NoopAnimationsModule,
     BrowserModule,
     CdkTreeModule,
     MatIconModule,
